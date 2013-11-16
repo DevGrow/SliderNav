9913d6a8ba13938ed7f0a37d65eb9027e4cff6ac
Tariq <tariq.abusheikh@gmail.com>
2013-11-15 20:16:32 -0600

    missed some files

--- alphabetNav.js
+++ alphabetNav.js
@@ -1,11 +1,11 @@
 /*
- *  AlphabetSoup - A jQuery navigation plugin for navigating and alphabetical list (ex: contacts, countries, etc)
+ *  AlphabetNav - A jQuery navigation plugin for navigating and alphabetical list (ex: contacts, countries, etc)
  *  Copyright 2013 Tariq Abusheikh, https://github.com/triq6
- *  Loosely based on the SliderNav plugin, developed by Monjurul Dolon, http://mdolon.com/
- *  For more information on SliderNav: http://devgrow.com/slidernav
+ *  Originally based on the SliderNav plugin, developed by Monjurul Dolon, http://mdolon.com/
+ *  For more information on SliderNav: http://devgrow.com/SliderNav
  *  Released under the MIT, BSD, and GPL Licenses.
  */
-$.fn.alphabetSoup = function (options) {
+$.fn.alphabetNav = function (options) {
     var defaults = {
             arrows: false,
             content: '.list-content',
@@ -20,40 +20,40 @@ $.fn.alphabetSoup = function (options) {
         },
         opts = $.extend(defaults, options),
         o = $.meta ? $.extend({}, opts, $$.data()) : opts,
-        slider = $(this),
-        sliderContent = o.content;
+        list = $(this),
+        listContent = o.content;
     if (o.overlay) {
-        $(slider).append('<a id="current-target-overlay"></a>');
+        $(list).append('<a id="current-target-overlay"></a>');
         var $overlay = $('#current-target-overlay');
     }
-    console.log("Slider content selector: " + sliderContent);
-    $(slider).addClass('slider');
-    $(sliderContent, slider).find('li:first').addClass('selected');
-    $(slider).append('<div class="slider-nav"><ul></ul></div>');
+    console.log("list content selector: " + listContent);
+    $(list).addClass('list');
+    $(listContent, list).find('li:first').addClass('selected');
+    $(list).append('<div class="list-nav"><ul></ul></div>');
     for (var i in o.items) {
-        $('.slider-nav ul', slider).append("<li><a data-target='" + o.items[i] + "'>" + o.items[i] + "</a></li>");
+        $('.list-nav ul', list).append("<li><a data-target='" + o.items[i] + "'>" + o.items[i] + "</a></li>");
     }
-    var height = $('.slider-nav', slider).height(),
-        currSize = parseInt($('.slider-nav a').css('font-size'), 10);
+    var height = $('.list-nav', list).height(),
+        currSize = parseInt($('.list-nav a').css('font-size'), 10);
     if (o.height) {
         height = o.height;
     }
-    $(sliderContent + ', slider-nav', slider).css('height', height);
+    $(listContent + ', list-nav', list).css('height', height);
     if (o.debug) {
-        $(slider).append('<div id="debug">Scroll Offset: <span>0</span></div>');
+        $(list).append('<div id="debug">Scroll Offset: <span>0</span></div>');
     }
-    $('.slider-nav a', slider).on('mouseover', function (evt) {
+    $('.list-nav a', list).on('mouseover', function (evt) {
         evt.preventDefault();
         var target = $(this).data('target'),
-            cOffset = $(sliderContent, slider).offset().top,
-            tOffset = $(sliderContent + ' #' + target, slider).offset().top,
-            height = $('.slider-nav', slider).height();
+            cOffset = $(listContent, list).offset().top,
+            tOffset = $(listContent + ' #' + target, list).offset().top,
+            height = $('.list-nav', list).height();
         console.log("target", target);
         if (o.height) {
             height = o.height;
         }
         var pScroll = (tOffset - cOffset) - height / 8;
-        $(sliderContent, slider).find('li').removeClass('selected');
+        $(listContent, list).find('li').removeClass('selected');
         $('#' + target).addClass('selected');
         if (o.overlay) {
             $overlay.html(target).show();
@@ -65,11 +65,11 @@ $.fn.alphabetSoup = function (options) {
                     fontSize : bigSize
             }, 100);
         }
-        $(sliderContent, slider).stop().animate({
+        $(listContent, list).stop().animate({
             scrollTop: '+=' + pScroll + 'px'
         });
         if (o.debug) {
-            $('#debug span', slider).html(tOffset);
+            $('#debug span', list).html(tOffset);
         }
     }).on('mouseleave', function (evt) {
         evt.preventDefault();
@@ -84,20 +84,20 @@ $.fn.alphabetSoup = function (options) {
             }
         }
     });
-    $('.slider-nav', slider).on('mouseleave', function () {
+    $('.list-nav', list).on('mouseleave', function () {
         if (o.overlay) {
             $overlay.fadeOut();
         }
     });
     if (o.arrows) {
-        $('.slider-nav', slider).css('top', '20px');
-        $(slider).prepend('<div class="slide-up end"><span class="arrow up"></span></div>');
-        $(slider).append('<div class="slide-down"><span class="arrow down"></span></div>');
-        $('.slide-down', slider).on('click', function () {
-            $(sliderContent, slider).animate({scrollTop: "+=" + height + "px"}, 500);
+        $('.list-nav', list).css('top', '20px');
+        $(list).prepend('<div class="slide-up end"><span class="arrow up"></span></div>');
+        $(list).append('<div class="slide-down"><span class="arrow down"></span></div>');
+        $('.slide-down', list).on('click', function () {
+            $(listContent, list).animate({scrollTop: "+=" + height + "px"}, 500);
         });
-        $('.slide-up', slider).on('click', function () {
-            $(sliderContent, slider).animate({scrollTop: "-=" + height + "px"}, 500);
+        $('.slide-up', list).on('click', function () {
+            $(listContent, list).animate({scrollTop: "-=" + height + "px"}, 500);
         });
     }
 };
